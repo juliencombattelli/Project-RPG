57fcf7c88a2449e50319231eb033996b5f637c71
juliencombattelli <julien.combattelli@hotmail.com>
2015-04-23 19:27:55 +0200

    Test movement without keys driver in CarreRouge.
    Try to fixe jerky movements. Unsuccessful ...

--- CarreRouge.cpp
+++ CarreRouge.cpp
@@ -1,5 +1,6 @@
 #include "CarreRouge.hpp"
 #include "Application.hpp"
+#include <SFML/Window/Keyboard.hpp>
 
 namespace rpg
 {
@@ -8,7 +9,7 @@ CarreRouge::CarreRouge(Application* app) : State(app), mRectangle(), mSpeed(150)
 {
     mRectangle.setSize(sf::Vector2f(50, 50));
     mRectangle.setFillColor(sf::Color::Red);
-    mRectangle.setPosition(10, 20);
+    mRectangle.setPosition(50, 50);
 }
 
 CarreRouge::~CarreRouge()
@@ -18,7 +19,7 @@ CarreRouge::~CarreRouge()
 
 void CarreRouge::handleEvent(sf::Event& event)
 {
-    if(sf::isKeyPressed(sf::Keyboard::P))
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::P))
     {
         mEvent.createState(StateID::MENU);
         LOGE(SeverityInfo, "State <MENU> created");
@@ -28,14 +29,53 @@ void CarreRouge::handleEvent(sf::Event& event)
 
 void CarreRouge::update(float elapsedTime)
 {
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        mRectangle.move( 0,-mSpeed*elapsedTime);
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        mRectangle.move( 0,+mSpeed*elapsedTime);
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        mRectangle.move(-mSpeed*elapsedTime, 0);
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        mRectangle.move(+mSpeed*elapsedTime, 0);
+    //if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    //    mRectangle.move( 0,-mSpeed*elapsedTime);
+    //if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    //    mRectangle.move( 0,+mSpeed*elapsedTime);
+    //if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    //    mRectangle.move(-mSpeed*elapsedTime, 0);
+    //if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    //    mRectangle.move(+mSpeed*elapsedTime, 0);
+    static int direction = 0;
+    switch(direction)
+    {
+        case 0:
+        {
+            if(mRectangle.getPosition().x>=700)
+                direction = 1;
+            else
+                mRectangle.move(+mSpeed*elapsedTime, 0);
+        }
+        break;
+
+        case 1:
+        {
+            if(mRectangle.getPosition().y>=500)
+                direction = 2;
+            else
+                mRectangle.move( 0,+mSpeed*elapsedTime);
+        }
+        break;
+
+        case 2:
+        {
+            if(mRectangle.getPosition().x<=50)
+                direction = 3;
+            else
+                mRectangle.move(-mSpeed*elapsedTime, 0);
+        }
+        break;
+
+        case 3:
+        {
+            if(mRectangle.getPosition().y<=50)
+                direction = 0;
+            else
+                mRectangle.move( 0,-mSpeed*elapsedTime);
+        }
+        break;
+    }
 }
 
 void CarreRouge::draw()
